80cf11f0deb5b4bf92831a0e4226ee5441e245df
Brent <poposhub@gmail.com>
2014-05-19 19:02:03 +0300

    Update on menu and added area

--- src/menu.py
+++ src/menu.py
@@ -1,42 +1,55 @@
-'''
-Menu System for game play. There should be a variety of different menus
-'''
+import os
+# This is the menu system for the game
+# The plan is to create custom menus where needed
 
-locations = ["The Field", "The Tavern", "Your House"]
 
+def display_title_bar():
+    #clears the terminal screen, and displays a title bar
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print "\t*******************************************"
+    print "\t***  Welcome - The game is an adventure ***"
+    print "\t*******************************************"
+
+
+def main_menu_choice():
+    print "\n[1] Start A New Game."
+    print "[2] Continue Your Game."
+    print "[3] Quit The Game."
+    return raw_input("What would you like to do? ")
+
+
+def travel_menu_choice():
+    print "\n[1] Enter 1 to explore"
+    print "[2] Enter 2 to run"
+    print "[3] Enter 3 to look around"
+    print "[4] Enter 4 to leave area"
+    print "[5] Enter 5 to return to main menu"
+    return raw_input("What would you like to do? ")
 
 
 def main_menu():
     '''Very basic main menu'''
+
     choice = ''
     while choice != '3':
-        print "\n[1] Enter 1 to travel."
-        print "[2] Enter 2 to explore."
-        print "[3] Enter 3 to quit."
-
-        choice = raw_input("\nWhat would you like to do? ")
-
+        choice = main_menu_choice()
+        display_title_bar()
         if choice == '1':
             travel_menu()
         elif choice == '2':
             print "You see a person standing next to you"
         elif choice == '3':
-            print "Thanks for playing"
+            exit(0)
         else:
             print "I don't understand, please try again"
+
 
 def travel_menu():
     '''Travel Menu'''
     choice = ''
     while choice != '5':
-        print "\n[1] Enter 1 to explore"
-        print "[2] Enter 2 to run"
-        print "[3] Enter 3 to look around"
-        print "[4] Enter 4 to leave area"
-        print "[5] Enter 5 to return to main menu"
-
-        choice = raw_input("\nWhat would you like to do? ")
-
+        choice = travel_menu_choice()
+        display_title_bar()
         if choice == '1':
             print "You explore the", locations[0], "but find nothing"
         elif choice == '2':
